d66ce435aed7063c5630b450b35d3fd1998370d1
dombroks <tinoutech@gmail.com>
2020-07-16 20:37:22 +0100

    Initial commit

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "Something went wrong. Please try again, Please Email me with the regular way"; }
 		
 	} # end if - no validation error
 
